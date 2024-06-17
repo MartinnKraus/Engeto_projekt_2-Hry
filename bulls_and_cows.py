65d6d6e1f5f9bc51528aae4b1828e23943d7c888
MartinnKraus <159342426+MartinnKraus@users.noreply.github.com>
2024-06-17 22:22:03 +0200

    Přidána fce pro formátování času

--- bulls_and_cows.py
+++ bulls_and_cows.py
@@ -5,9 +5,28 @@ email: martinnkraus@gmail.com
 discord: martin_64789
 """
 
-import random, time, sys
+import random, time
 
 random.seed(0)
+
+def pretty_time_delta(seconds: float) -> str:
+    """
+    Vrátí naformátovaný čas ve stringu, vstup v sekundách
+    """
+    sign_string = '-' if seconds < 0 else ''
+    seconds = abs(int(seconds))
+    days, seconds = divmod(seconds, 86400)
+    hours, seconds = divmod(seconds, 3600)
+    minutes, seconds = divmod(seconds, 60)
+    if days > 0:
+        return '%s%dd%dh%dm%ds' % (sign_string, days, hours, minutes, seconds)
+    elif hours > 0:
+        return '%s%dh%dm%ds' % (sign_string, hours, minutes, seconds)
+    elif minutes > 0:
+        return '%s%dm%ds' % (sign_string, minutes, seconds)
+    else:
+        return '%s%ds' % (sign_string, seconds)
+
 
 def generuj_nahodne_unikatni() -> str:
     """
